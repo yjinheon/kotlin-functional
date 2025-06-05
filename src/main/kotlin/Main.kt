107d43b406b64dfc25e6519d54e3dec0e933ff5b
jinheonyoon <yjinheon@gmail.com>
2025-06-05 15:31:34 +0900

    feat: build spike

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,35 @@
 package com.example
 
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+import org.http4k.server.asServer
+import org.http4k.server.Jetty
+import org.http4k.core.Status.Companion.CREATED
+import org.http4k.core.Status.Companion.OK
+
+val htmlPage = """
+  <html>
+  <body>
+  <h1 style="text-align: font-size:3em;">Hello Functional World!</h1>
+  </body>
+  </html>
+""".trimIndent()
+
+
+val app: HttpHandler = routes(
+    "/greetings" bind Method.GET to ::greetings,
+    "/data" bind Method.POST to ::receiveData,
+)
+
+fun greetings(req: Request): Response = Response(OK).body(htmlPage)
+
+fun receiveData(req: Request): Response = Response(CREATED).body("Received data : ${req.bodyString()}")
+
+
 fun main() {
-    println("Hello World!")
+    app.asServer(Jetty(8080)).start()
 }
